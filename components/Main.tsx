322f992cf0a11629ee48996b3b0684d70c6b41a8
Josh Pollock <jpollock412@gmail.com>
2020-11-07 11:24:30 -0500

    main

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,6 +1,6 @@
 export default ({ children }) => {
   return (
-    <main className="antialiased text-gray-900 flex items-center justify-center min-h-screen">
+    <main className="flex-auto relative border-t border-gray-200 dark:border-gray-800">
       { children }
     </main>
   )
