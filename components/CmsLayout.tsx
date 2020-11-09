1a193f45c91db5ac72e20d786d9f62ec52a55450
Josh Pollock <jpollock412@gmail.com>
2020-11-09 17:23:48 -0500

    cms topbar

--- components/CmsLayout.tsx
+++ components/CmsLayout.tsx
@@ -1,7 +1,29 @@
+import Link from 'next/link';
 import React, { FC, ReactNode, useEffect } from 'react';
 import Cms from './Cms';
 import Main from './Main';
 
+let navItems :{ to: string, label: string }[] = [
+    { to: '/cms/files', label: 'Item List' },
+    { to: '/cms/new', label: 'New Item' },
+]
+const CmsHeader = () => (
+    <header className={'container mx-auto flex-auto relative border-t border-gray-200 dark:border-gray-800'}>
+
+
+        <ul className="flex">
+            {navItems.map(({ label, to }) => (
+                <li className="mr-6">
+                    <Link href={to}>
+                        <a className="text-blue-500 hover:text-blue-800" href="#">
+                            {label}
+                        </a>
+                    </Link> 
+              </li>
+            ))}
+            </ul>
+    </header>
+)
 const CmsLayout: FC<{ children: ReactNode, TopBar: () => JSX.Element }> = ({ children, TopBar }) => {
     
     useEffect(() => {
@@ -10,12 +32,15 @@ const CmsLayout: FC<{ children: ReactNode, TopBar: () => JSX.Element }> = ({ chi
     },[])
 
     return (
-        <Main>
-            <Cms>
-                <TopBar />
-                {children}
-            </Cms>      
-        </Main>
+        <>
+                <CmsHeader />
+              <Main>
+                <Cms>
+                    <TopBar />
+                    {children}
+                </Cms>      
+            </Main>
+        </>
     )
 };
 
