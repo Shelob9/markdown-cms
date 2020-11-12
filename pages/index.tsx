6f7a273bf64151ab1c223e46356ae4b174c18384
Josh Pollock <jpollock412@gmail.com>
2020-11-12 17:38:34 -0500

    test session load on front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import Main from '../components/Main'
+import { useSession } from 'next-auth/client'
 
-const Index = () => (
-  <Main>
-    <h1 className="text-5xl font-bold text-purple-500">Hello!</h1>
-  </Main>
-)
+const Index = () => {
+  const [ session, loading ] = useSession()
+  return (
+    <Main>
+      <h1 className={`text-5xl font-bold text-purple-500 ${loading ? 'animate-pulse': ''}`}>
+    
+        Hi {session && session.user ? session.user.name : 'Roy'}
+      </h1>
+    </Main>
+  )
+}
 
 export default Index;
